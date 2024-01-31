1b4e66ec1aa360641fe2481fc6e2482578c6d585
HavallonTech <kestercode@gmail.com>
2024-02-01 00:29:20 +0100

    Delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Delete a binary tree
+ * @tree: the given tree
+ *
+ * Return: void
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+
+}
