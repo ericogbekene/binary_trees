3dafe70102d28b2adc277c645e10172045c56fcc
HavallonTech <kestercode@gmail.com>
2024-02-01 21:31:03 +0100

    Binary height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,24 +6,30 @@
  *
  * Return: the height if the tree
  */
-size_t height = 0;
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 /* strategy: check if a node has child */
 /*the increament the count using recursion*/
+	size_t rgt, lft;
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	int rgt =  binary_tree_height(tree->right);
-	int lft =  binary_tree_height(tree->left);
-	if (lft > rgt)
+	if (tree->right)
 	{
-		height = lft + 1;
+		rgt =  1 + binary_tree_height(tree->right);
 	}
 	else
 	{
-		height = rgt +1;
+		rgt = 0;
 	}
-	return (height);
+	if (tree->left)
+	{
+		lft = 1 + binary_tree_height(tree->left);
+	}
+	else
+	{
+		lft = 0;
+	}
+	return ((lft > rgt) ? lft : rgt);
 }
