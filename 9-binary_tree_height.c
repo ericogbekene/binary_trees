041a0e970578683bfcbfcffdaeb2172d00dc78cd
HavallonTech <kestercode@gmail.com>
2024-02-01 21:35:39 +0100

    Binary height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,6 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 /* strategy: check if a node has child */
 /*the increament the count using recursion*/
 	size_t rgt, lft;
+
 	if (tree == NULL)
 	{
 		return (0);
