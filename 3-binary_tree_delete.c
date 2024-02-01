d848ce306851b66f350d68855eae459d6cf36823
HavallonTech <kestercode@gmail.com>
2024-02-01 02:05:37 +0100

    Delete using recurssion

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -8,5 +8,14 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
+/* strategy: Post order traversal and calling thr free function */
+/*Post order : visiting the left subtree, right subtree and checking the node*/
+	if (tree == NULL)
+	{
+		return;
+	}
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 
 }
