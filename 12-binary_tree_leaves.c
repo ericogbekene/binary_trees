26e1d55f16796c6018086bad0a0a7fad3f09a3d3
HavallonTech <kestercode@gmail.com>
2024-02-02 01:28:14 +0100

    Binary tree

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leave - count the leave of a binary tree
+ * @tree: the given tree to count
+ *
+ * Return: the size if the tree leaves
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+/* strategy: check if a node has child */
+/*the increament the count using recursion*/
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left == NULL && tree->right== NULL)
+		return 1;
+	else
+	{
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	}
+}
--- binary_trees.h
+++ binary_trees.h
@@ -31,5 +31,8 @@ int binary_tree_is_leaf(const binary_tree_t *node);
 size_t binary_tree_height(const binary_tree_t *tree);
 /* Prototype for print functions */
 void binary_tree_print(const binary_tree_t *tree);
-
+size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
 #endif /*BINARY_TREES_H*/
