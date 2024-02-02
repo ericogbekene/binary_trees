b2aa5634c87611a8069fa53715b9d7b821a5bba3
Eric Ogbekene <ericogbekene@icloud.com>
2024-02-01 18:07:38 -0800

    12-binary_tree_leaves betty correction

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leave - count the leave of a binary tree
+ * binary_tree_leaves - count the leave of a binary tree
  * @tree: the given tree to count
  *
  * Return: the size if the tree leaves
@@ -15,10 +15,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	if (tree->left == NULL && tree->right== NULL)
-		return 1;
-	else
-	{
-		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
-	}
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
